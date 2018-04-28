b725dd6395affc1a105996f5d994b3e4eb8fbab8
stevekibe <stevekibe2345@gmail.com>
2018-04-29 02:02:14 +0300

    add the changes in the html and the css file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HomeFormComponent } from './home-form/home-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeFormComponent
   ],
   imports: [
     BrowserModule
